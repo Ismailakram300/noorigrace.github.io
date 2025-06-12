6031a54cc7229ab766f0f5400935fb09d99dae92
Ismailakram300 <123332697+Ismailakram300@users.noreply.github.com>
2025-06-12 16:22:30 +0500

    Update admin_header.php

--- admin_header.php
+++ admin_header.php
@@ -2,7 +2,6 @@
 if(isset($message)){
    foreach($message as $message){
       echo '
-      
       <div class="message">
          <span>'.$message.'</span>
          <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
